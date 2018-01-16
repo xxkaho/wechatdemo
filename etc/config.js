235d396bfef5fbfef249ccc761c81a580f9c3103
xxkaho <137542293@qq.com>
2018-01-16 13:33:20 +0800

    12312

---
+++ etc/config.js
@@ -0,0 +1,8 @@
+export default {
+	// 线上环境
+	// basePath: 'https://www.skyvow.cn/api', 
+	// domain: 'https://www.skyvow.cn/', 
+	// 本地环境
+  basePath: 'http://10.236.52.133:3000/api', 
+  domain: 'http://10.236.52.133:3000/', 
+}
